090d72e057246b05b4322af5e52e2a6658509034
Tommy Campbell <tjc2187@gmail.com>
2019-07-01 18:00:02 -0500

    created basic structure

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:root@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Blog(db.Model):  
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(500))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+@app.route('/blog')
+def index():
+    return render_template('blog.html', title='Build A Blog!')
+
+
+
+if __name__ == '__main__':
+    app.run()
