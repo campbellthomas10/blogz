82bde0eb75124ea81c01d43c760be8bf9610b462
Tommy Campbell <tjc2187@gmail.com>
2019-07-01 20:01:30 -0500

    finished create post and displaying created posts

--- main.py
+++ main.py
@@ -19,8 +19,62 @@ class Blog(db.Model):
 
 @app.route('/blog')
 def index():
-    return render_template('blog.html', title='Build A Blog!')
 
+    posts = Blog.query.all()
+
+    return render_template('blog.html', title='Build A Blog', posts=posts)
+
+#Checks if post fields (title or body) are blank
+def validate_field(field):
+    a_field = field.strip()
+    return a_field == ''
+
+
+@app.route('/newpost', methods=['POST','GET'])
+def new_post():
+
+    title_error = ''
+    body_error = ''
+    post_title = ''
+    post_body = ''
+
+    #Runs if a new post is submitted
+    if request.method == 'POST':
+
+        post_title = request.form['title']
+        post_body = request.form['body']
+
+        if validate_field(post_title):
+            title_error = "Please enter a title!"
+            post_title = ''
+        if validate_field(post_body):
+            body_error = "Please enter the body contents!"
+            post_body = ''
+
+
+
+        if not validate_field(post_title) and not validate_field(post_body):
+
+            post = Blog(post_title,post_body)
+            db.session.add(post)
+            db.session.commit()
+            return redirect('/blog')
+        
+        else:
+            return render_template('newpost.html',
+                title='New Post',
+                title_error=title_error,
+                body_error=body_error,
+                post_title=post_title,
+                post_body=post_body)
+
+
+        return render_template('newpost.html',
+            title='New Post',
+            title_error=title_error,
+            body_error=body_error,
+            post_title=post_title,
+            post_body=post_body)
 
 
 if __name__ == '__main__':
